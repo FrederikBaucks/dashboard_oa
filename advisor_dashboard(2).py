b38db582f8aec41e82ebd3438e09105a31290eb8
FrederikBaucks <93192221+FrederikBaucks@users.noreply.github.com>
2025-04-03 15:45:48 +0200

    changed file name st render recognizes main file..

---
+++ advisor_dashboard(2).py
@@ -0,0 +1,37 @@
+import dash
+from dash import Dash, dcc, html, Output, Input
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+app = Dash(__name__)
+
+# SQLAlchemy engine with connection pooling
+engine = create_engine("sqlite:///university.db", pool_size=5, max_overflow=10)
+
+app.layout = html.Div([
+    dcc.Dropdown(id="course-dropdown", options=[]),
+    html.Div(id="output"),
+    dcc.Textarea(
+        placeholder='Enter a value...',
+        value='This is a TextArea component',
+        style={'width': '100%'}
+    )
+])
+
+@app.callback(
+    Output("output", "children"),
+    Input("course-dropdown", "value")
+)
+def update_output(selected_course):
+    with engine.connect() as conn:
+        query = "SELECT * FROM Courses"
+        df = pd.read_sql(query, conn)
+    
+    return f"Data Loaded: {len(df)} rows"
+
+#if __name__ == "__main__":
+#    app.run_server(debug=True)
+
+# app = dash.Dash(__name__)     
+server = app.server
--- advisor_dashboard.py
+++ advisor_dashboard.py
@@ -1,37 +1,35 @@
-import dash
-from dash import Dash, dcc, html, Output, Input
-from sqlalchemy import create_engine
-import pandas as pd
+#from app import app
 
-
-app = Dash(__name__)
-
-# SQLAlchemy engine with connection pooling
-engine = create_engine("sqlite:///university.db", pool_size=5, max_overflow=10)
-
-app.layout = html.Div([
-    dcc.Dropdown(id="course-dropdown", options=[]),
-    html.Div(id="output"),
-    dcc.Textarea(
-        placeholder='Enter a value...',
-        value='This is a TextArea component',
-        style={'width': '100%'}
-    )
-])
-
-@app.callback(
-    Output("output", "children"),
-    Input("course-dropdown", "value")
-)
-def update_output(selected_course):
-    with engine.connect() as conn:
-        query = "SELECT * FROM Courses"
-        df = pd.read_sql(query, conn)
+from layout import app
+from callbacks import *
+from components import *
+# import networkx as nx
+# def get_recent_courses():
+#     '''
+#     This function construct a networkx graph for each student and saves it as an image in /assets/graphs/
+#     '''
+#     # Load the data
+#     student_data = pd.read_pickle(r'data/AI_raw.pickle')
+#     ids = [x.name for x in student_data.students]
+#     # If folder assets/graphs/ does not exist, create it
+#     if not os.path.exists('assets/graphs/'):
+#         os.makedirs('assets/graphs/')
+#     # For each student, construct a graph and save it as an image
+#     for i,id in enumerate(ids):
+#         # If graph already exists, skip
+#         if os.path.exists('assets/graphs/'+id+'.png'):
+#             continue
+#         # Construct random networkx graph
+#         G = nx.fast_gnp_random_graph(20, 0.5)
+#         # Save graph as image
+#         nx.draw(G, with_labels=True, font_weight='bold')
+#         plt.savefig('assets/graphs/'+id+'.png')
+#         plt.clf()
+        
     
-    return f"Data Loaded: {len(df)} rows"
-
-#if __name__ == "__main__":
-#    app.run_server(debug=True)
-
-# app = dash.Dash(__name__)     
 server = app.server
+
+if __name__ == '__main__':
+    #get_recent_courses()
+    
+    app.run_server(debug=True)
